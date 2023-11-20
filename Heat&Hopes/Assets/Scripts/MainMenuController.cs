cf2a1c15d1a7ccc5d04307d46ff3c373be885f4d
Alejandro Pérez Carretero <96301176+AlexPC03@users.noreply.github.com>
2023-11-20 15:18:10 +0100

    prototipo menú inicio

---
+++ Heat&Hopes/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void MainMenuChangeScene(int button) //funcion para el cambio de escena
+    {
+        switch (button)
+        {
+            case 0:
+                //Cargar escena de juego
+                SceneManager.LoadScene("Game");
+                break;
+            case 1:
+                //Cargar escena de ajustes
+                SceneManager.LoadScene("Carlos' Testing Scene");
+                break;
+            case 2:
+                //Cargar escena de accesibilidad
+                SceneManager.LoadScene("Dyslexia Test");
+                break;
+            case 3:
+                //Salir
+                Application.Quit();
+                break;
+
+        }
+    }
+}
