732f3e34b0ab356b435260d659d6fb766bd46373
Mdoc14 <marcos.deozaeta@gmail.com>
2023-11-24 19:26:11 +0100

    Más sprites y efecto paralax

---
+++ Heat&Hopes/Assets/StarshipSpawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarshipSpawn : MonoBehaviour
+{
+    public GameObject player;
+    public float t;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
