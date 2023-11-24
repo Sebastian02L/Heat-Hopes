3bb2d85335498f953ed1005d9e5d0502a1071d86
Alejandro Pérez Carretero <96301176+AlexPC03@users.noreply.github.com>
2023-11-24 14:17:30 +0100

    Barra de carga de menú de inicio

--- Heat&Hopes/Assets/Scripts/MainMenuController.cs
+++ Heat&Hopes/Assets/Scripts/MainMenuController.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject loadOverlay;
+    public Image loadBar;
+    private bool loading;
+
+    private void Start()
+    {
+        loadOverlay.SetActive(false);
+    }
 
     public void MainMenuChangeScene(int button) //funcion para el cambio de escena
     {
@@ -12,14 +21,19 @@ public class MainMenuController : MonoBehaviour
         {
             case 0:
                 //Cargar escena de juego
-                SceneManager.LoadScene("TutorialScene");
+                if (!loading)
+                {
+                    StartCoroutine(LoadNextScene());
+                    loading = true;
+                    loadOverlay.SetActive(true);
+                }
                 break;
             case 1:
-                //Cargar escena de ajustes
+                //Cargar escena de ajustes, debería ser una parte del canvas
                 SceneManager.LoadScene("Carlos' Testing Scene");
                 break;
             case 2:
-                //Cargar escena de accesibilidad
+                //Cargar escena de accesibilidad, debería ser una parte del canvas
                 SceneManager.LoadScene("Dyslexia Test");
                 break;
             case 3:
@@ -27,6 +41,17 @@ public class MainMenuController : MonoBehaviour
                 Application.Quit();
                 break;
 
+        }
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        AsyncOperation loadLevel = SceneManager.LoadSceneAsync("TutorialScene");
+
+        while (!loadLevel.isDone) 
+        {
+            loadBar.fillAmount = Mathf.Clamp01(loadLevel.progress / 0.9f);
+            yield return null;
         }
     }
 }
