7c2aaf4766242736ed37de8668aa873f57e276fe
carlossantayana <carlossantayanavicente@gmail.com>
2023-11-24 18:31:36 +0100

    Habilidad especial botas terminada. Excepto...
    Faltaría modificar la condición de recarga

--- Heat&Hopes/Assets/Scripts/ItemRelated/Boots.cs
+++ Heat&Hopes/Assets/Scripts/ItemRelated/Boots.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Boots : Item
 {
-    private float _maxEnergy = 4f;
+    private float _maxEnergy = 2f;
     private float _currEnergy;
     private float _thrustForce = 0.1f;
 
@@ -48,15 +48,31 @@ public class Boots : Item
             used = true;
 
             _currEnergy -= Time.deltaTime;
-            _playerRigidBody.AddForce(_playerRigidBody.transform.up * _thrustForce, ForceMode2D.Impulse);
 
-            Debug.Log($"Habilidad de {itemName} usada.");
+            if (_currEnergy > 0.1)
+            {
+                _playerRigidBody.AddForce(_playerRigidBody.transform.up * _thrustForce, ForceMode2D.Impulse);
+            }
+
+            Debug.Log($"Habilidad de {itemName} en uso. Gastando energía...");
         }
-        else //Creo que esto no debería ir aquí ya que la energía se consigue explorando el mundo
+        else if (_abilityKeyPressed && _currEnergy <= 0) 
+        {
+            _currEnergy = 0;
+            Debug.Log("Sin energía");
+        }
+
+        if(_player.grounded == true) //Creo que esto no debería ir aquí ya que la energía se consigue explorando el mundo
         {
             if (_currEnergy < _maxEnergy)
             {
                 _currEnergy += Time.deltaTime;
+                Debug.Log("Recargando Energía");
+            }
+            else
+            {
+                _currEnergy = _maxEnergy;
+                Debug.Log("Energía máxima alcanzada");
             }
         }
     }
